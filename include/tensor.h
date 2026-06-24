1c5f5b1799850e4295e0552723ece2dd0d543b5e
VladPrais <zzztiop9@gmail.com>
2026-06-24 21:03:00 +0300

    The first commit

---
+++ include/tensor.h
@@ -0,0 +1,414 @@
+#ifndef TENSOR_H
+#define TENSOR_H
+
+#include <array>
+#include <cmath>
+#include <vector>
+#include <memory>
+
+#ifdef TENSOR_TEST
+
+#include <iostream>
+
+#define PRINT(x) do{ std::cout << x << std::endl; }while(0)
+
+#define TEST_DEF_INIT "Default constructor"		// class()
+#define TEST_ARG_INIT "Constructor using argument" 	// class(params)
+#define TEST_COPY_INIT "Copy constructor"		// class(const class&)
+#define TEST_MOVE_INIT "Move constructor"		// class(class&&)
+
+#define TEST_COPY_ASSIGNMENT "Copy assignment"		// operator=(const class&)
+#define TEST_MOVE_ASSIGNMENT "Move assignment"		// operator=(class&&)
+
+#define TEST_ADD_ASSIGNMENT "Addition assignment"	// operator+=(const class&)
+#define TEST_SUB_ASSIGNMENT "Subtraction assignment"	// operator-=(const class&)
+#define TEST_MUL_ASSIGNMENT "Multiplication assignment"	// operator*=(const class&)
+#define TEST_DIV_ASSIGNMENT "Division assignment"	// operator/=(consr class&)
+
+#define TEST_ADD "Addition"				// operator+(class, const class&)
+#define TEST_SUB "Subtraction"				// operator-(class, const class&)
+#define TEST_MUL "Multiplication"			// operator*(class, const class&)
+#define TEST_DIV "Division"				// operator/(class, const class&)
+
+#define TEST_EQUAL "Equal"				// operator==(const class&)
+#define TEST_NOT_EQUAL "Not equal"			// operator!=(const class&)
+
+#endif 
+
+namespace sl
+{
+
+template <typename T>
+class tensor
+{
+	std::unique_ptr<T[]> data;
+	std::unique_ptr<size_t[]> shape;
+	size_t size;
+	size_t rang;
+
+	public:
+
+	tensor(): data(nullptr), shape(nullptr), size(0), rang(0)
+	{
+#ifdef TENSOR_TEST
+		PRINT(TEST_DEF_INIT);
+#endif
+	}
+
+	tensor(const std::vector<size_t>& shape)
+	{
+#ifdef TENSOR_TEST
+		PRINT(TEST_ARG_INIT);
+#endif
+		size_t rang = shape.size();
+		this->size = 1;
+		this->rang = rang;
+		this->shape = std::make_unique<size_t[]>(this->rang);
+		for(size_t i = 0; i < rang; i++)
+		{
+			this->shape[i] = shape[i];
+			this->size *= shape[i];
+		}
+
+		this->data = std::make_unique<T[]>(this->size);
+	}
+
+	tensor(const tensor<T>& other)
+	{
+#ifdef TENSOR_TEST
+		PRINT(TEST_COPY_INIT);
+#endif
+		this->size = other.size;
+		this->rang = other.rang;
+		this->shape = std::make_unique<size_t[]>(this->rang);
+		this->data = std::make_unique<T[]>(this->size);
+
+		for(size_t i = 0; i < this->rang; i++)
+		{
+			this->shape[i] = other.shape[i];
+		}
+		
+		for(size_t i = 0; i < this->size; i++)
+		{
+			this->data[i] = other.data[i];
+		}
+	}
+
+	tensor(tensor<T>&& other)
+	{
+#ifdef TENSOR_TEST
+		PRINT(TEST_MOVE_INIT);
+#endif
+		this->size = other.size;
+		this->rang = other.rang;
+		this->shape = std::move(other.shape);
+		this->data = std::move(other.data);
+		other.size = 0;
+		other.rang = 0;
+	}
+
+	tensor<T>& operator=(const tensor<T>& other)
+	{
+#ifdef TENSOR_TEST
+		PRINT(TEST_COPY_ASSIGNMENT);
+#endif
+		if(this->rang != other.rang)
+		{
+			this->rang = other.rang;
+			std::unique_ptr<size_t[]> temp_ptr = std::make_unique<size_t[]>(this->rang);
+			this->shape = std::move(temp_ptr);
+		}
+		if(this->size != other.size)
+		{
+			this->size = other.size;
+			std::unique_ptr<T[]> temp_ptr = std::make_unique<T[]>(this->size);
+			this->data = std::move(temp_ptr);
+		}
+		for(size_t i = 0; i < this->rang; i++)
+		{
+			this->shape[i] = other.shape[i];
+		}
+		for(size_t i = 0; i < this->size; i++)
+		{
+			this->data[i] = other.data[i];
+		}
+		return *this;
+	}
+
+	tensor<T>& operator=(tensor<T>&& other)
+	{
+#ifdef TENSOR_TEST
+		PRINT(TEST_MOVE_ASSIGNMENT);
+#endif
+		this->size = other.size;
+		this->rang = other.rang;
+		this->data = std::move(other.data);
+		this->shape = std::move(other.shape);
+		other.size = 0;
+		other.rang = 0;
+		return *this;
+	}
+
+	tensor<T>& operator+=(const tensor<T>& other)
+	{
+#ifdef TENSOR_TEST
+		PRINT(TEST_ADD_ASSIGNMENT);
+#endif
+		if(!same_tensor(other))
+		{
+			throw;
+		}
+		for(size_t i = 0; i < this->size; i++)
+		{
+			this->data[i] += other.data[i];
+		}
+		return *this;
+	}
+
+	tensor<T>& operator-=(const tensor<T>& other)
+	{
+#ifdef TENSOR_TEST
+		PRINT(TEST_SUB_ASSIGNMENT);
+#endif
+		if(!same_tensor(other))
+		{
+			throw;
+		}
+		for(size_t i = 0; i < this->size; i++)
+		{
+			this->data[i] -= other.data[i];
+		}
+		return *this;
+	}
+
+	tensor<T>& operator*=(const tensor<T>& other)
+	{
+#ifdef TENSOR_TEST
+		PRINT(TEST_MUL_ASSIGNMENT);
+#endif
+		if(!same_tensor(other))
+		{
+			throw;
+		}
+		for(size_t i = 0; i < this->size; i++)
+		{
+			this->data[i] *= other.data[i];
+		}
+		return *this;
+	}
+
+	tensor<T>& operator/=(const tensor<T>& other)
+	{
+#ifdef TENSOR_TEST
+		PRINT(TEST_DIV_ASSIGNMENT);
+#endif
+		if(!same_tensor(other))
+		{
+			throw;
+		}
+		for(size_t i = 0; i < this->size; i++)
+		{
+			this->data[i] /= other.data[i];
+		}
+		return *this;
+	}
+
+	template <typename Constant>
+	tensor<T>& operator+=(Constant x)
+	{
+#ifdef TENSOR_TEST
+		PRINT(TEST_ADD_ASSIGNMENT);
+#endif
+		for(size_t i = 0; i < this->size; i++)
+		{
+			this->data[i] += x;
+		}
+		return *this;
+	}
+
+	template <typename Constant>
+	tensor<T>& operator-=(Constant x)
+	{
+#ifdef TENSOR_TEST
+		PRINT(TEST_SUB_ASSIGNMENT);
+#endif
+		for(size_t i = 0; i < this->size; i++)
+		{
+			this->data[i] -= x;
+		}
+		return *this;
+	}
+
+	template <typename Constant>
+	tensor<T>& operator*=(Constant x)
+	{
+#ifdef TENSOR_TEST
+		PRINT(TEST_MUL_ASSIGNMENT);
+#endif
+		for(size_t i = 0; i < this->size; i++)
+		{
+			this->data[i] *= x;
+		}
+		return *this;
+	}
+
+	template <typename Constant>
+	tensor<T>& operator/=(Constant x)
+	{
+#ifdef TENSOR_TEST
+		PRINT(TEST_DIV_ASSIGNMENT);
+#endif
+		for(size_t i = 0; i < this->size; i++)
+		{
+			this->data[i] /= x;
+		}
+		return *this;
+	}
+
+	T& operator[](size_t n)
+	{
+		return this->data[n];
+	}
+
+	template <typename Constant>
+	friend tensor<T> operator+(tensor<T> t, Constant x)
+	{
+#ifdef TENSOR_TEST
+		PRINT(TEST_ADD);
+#endif
+		t += x;
+		return std::move(t);
+	}
+
+	template <typename Constant>
+	friend tensor<T> operator+(Constant x, tensor<T> t)
+	{
+#ifdef TENSOR_TEST
+		PRINT(TEST_ADD);
+#endif
+		t += x;
+		return std::move(t);
+	}
+
+	template <typename Constant>
+	friend tensor<T> operator-(tensor<T> t, Constant x)
+	{
+#ifdef TENSOR_TEST
+		PRINT(TEST_SUB);
+#endif
+		t -= x;
+		return std::move(t);
+	}
+
+	template <typename Constant>
+	friend tensor<T> operator-(Constant x, tensor<T> t)
+	{
+#ifdef TENSOR_TEST
+		PRINT(TEST_SUB);
+#endif
+		t -= x;
+		return std::move(t);
+	}
+
+	template <typename Constant>
+	friend tensor<T> operator*(tensor<T> t, Constant x)
+	{
+#ifdef TENSOR_TEST
+		PRINT(TEST_MUL);
+#endif
+		t *= x;
+		return std::move(t);
+	}
+
+	template <typename Constant>
+	friend tensor<T> operator*(Constant x, tensor<T> t)
+	{
+#ifdef TENSOR_TEST
+		PRINT(TEST_MUL);
+#endif
+		t *= x;
+		return std::move(t);
+	}
+
+	template <typename Constant>
+	friend tensor<T> operator/(tensor<T> t, Constant x)
+	{
+#ifdef TENSOR_TEST
+		PRINT(TEST_DIV);
+#endif
+		t /= x;
+		return std::move(t);
+	}
+
+	template <typename Constant>
+	friend tensor<T> operator/(Constant x, tensor<T> t)
+	{
+#ifdef TENSOR_TEST
+		PRINT(TEST_DIV);
+#endif
+		t /= x;
+		return std::move(t);
+	}
+
+	bool operator==(const tensor<T>& other)
+	{
+#ifdef TENSOR_TEST
+		PRINT(TEST_EQUAL);
+#endif
+		if(this->size != other.size)
+		{
+			return false;
+		}
+		if(this->rang != other.rang)
+		{
+			return false;
+		}
+		for(size_t i = 0; i < this->rang; i++)
+		{
+			if(this->shape[i] != other.shape[i])
+			{
+				return false;
+			}
+		}
+		for(size_t i = 0; i < this->size; i++)
+		{
+			if(std::abs(this->data[i] - other.data[i]) > 1e-9)
+			{
+				return false;
+			}
+		}
+		return true;
+	}
+
+	bool operator!=(const tensor<T>& other)
+	{
+#ifdef TENSOR_TEST
+		PRINT(TEST_NOT_EQUAL);
+#endif
+		return !(*this == other);
+	}
+
+	~tensor() = default;
+
+	private:
+
+	bool same_tensor(const tensor<T>& other)
+	{
+		if(this->rang != other.rang)
+		{
+			false;
+		}
+		for(size_t i = 0; i < this->rang; i++)
+		{
+			if(this->shape[i] != other.shape[i])
+			{
+				false;
+			}
+		}
+		return true;
+	}
+};
+
+} // namespace sl
+
+#endif
---
+++ test/tensor_test.cpp
@@ -0,0 +1,36 @@
+#define TENSOR_TEST
+
+#include "tensor.h"
+
+#define TEST(x) std::cout << #x << std::endl; x; std::cout << std::endl;
+#define SPACE std::cout << std::endl;
+
+using sl::tensor;
+
+int main(void)
+{
+	TEST(tensor<int> t1)
+	TEST(tensor<int> t2({3, 3}))
+	TEST(tensor<int> t3(t2))
+	TEST(tensor<int> t4(std::move(t1)))
+	TEST(tensor<int> t5 = t4)
+	TEST(tensor<int> t6 = std::move(t2))
+	TEST(t2 = t3 = t4)
+	
+	TEST(t3 += 1)
+	TEST(t3 -= 1)
+	TEST(t3 *= 1)
+	TEST(t3 /= 1)
+
+	TEST(t3 = t4 + 1)
+	TEST(t3 = t4 - 1)
+	TEST(t3 = t4 * 1)
+	TEST(t3 = t4 / 1)
+
+	TEST(t3 = 1 + t4)
+	TEST(t3 = 1 - t4)
+	TEST(t3 = 1 * t4)
+	TEST(t3 = 1 / t4)
+
+	return 0;
+}
